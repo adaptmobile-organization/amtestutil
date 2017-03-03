d59f332c4b031f33fce470e85242a076c73f354a
Bjarke Severinsen <bjarkeseverinsen@Bjarkes-MBP.lbg.adapt.dk>
2017-03-03 16:02:55 +0100

    Scrollview update

--- app/src/main/java/dk/adaptmobile/amtestutil/uitest/scrollview/AMScrollViewTestUtil.java
+++ app/src/main/java/dk/adaptmobile/amtestutil/uitest/scrollview/AMScrollViewTestUtil.java
@@ -4,6 +4,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static dk.adaptmobile.amtestutil.uitest.general.AMUITestUtil.findViewById;
 
 /**
  * Created by bjarkeseverinsen on 27/02/2017.
@@ -11,12 +12,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class AMScrollViewTestUtil {
 
-    public static void scrollToView(int view){
-        onView(withId(view)).perform(scrollTo());
+    public static void scrollToView(int id){
+        findViewById(id).perform(scrollTo());
     }
 
-    public static void scrollToViewAndClick(int view){
-        onView(withId(view)).perform(scrollTo(), click());
+    public static void scrollToViewAndClick(int id){
+        findViewById(id).perform(scrollTo(), click());
     }
 
 }
